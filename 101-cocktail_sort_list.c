b7aa6720e2f111602591bb475e046601940598e2
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-06-28 03:53:35 +0300

    Task 5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -16,40 +16,40 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *start = NULL;
-    listint_t *end = NULL;
-    int swapped = 0;
+listint_t *start = NULL;
+listint_t *end = NULL;
+int swapped = 0;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
 
-    do {
-        swapped = 0;
-        for (start = *list; start->next != end; start = start->next)
-        {
-            if (start->n > start->next->n)
-            {
-                swap_nodes(list, start, start->next);
-                swapped = 1;
-                print_list(*list);
-            }
-        }
+do {
+swapped = 0;
+for (start = *list; start->next != end; start = start->next)
+{
+if (start->n > start->next->n)
+{
+swap_nodes(list, start, start->next);
+swapped = 1;
+print_list(*list);
+}
+}
 
-        if (!swapped)
-            break;
+if (!swapped)
+break;
 
-        swapped = 0;
-        end = start;
-        for (; start->prev != NULL; start = start->prev)
-        {
-            if (start->prev->n > start->n)
-            {
-                swap_nodes(list, start->prev, start);
-                swapped = 1;
-                print_list(*list);
-            }
-        }
-    } while (swapped);
+swapped = 0;
+end = start;
+for (; start->prev != NULL; start = start->prev)
+{
+if (start->prev->n > start->n)
+{
+swap_nodes(list, start->prev, start);
+swapped = 1;
+print_list(*list);
+}
+}
+} while (swapped);
 }
 
 /**
@@ -60,16 +60,16 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    if (node1->prev != NULL)
-        node1->prev->next = node2;
-    else
-        *list = node2;
+if (node1->prev != NULL)
+node1->prev->next = node2;
+else
+*list = node2;
 
-    if (node2->next != NULL)
-        node2->next->prev = node1;
+if (node2->next != NULL)
+node2->next->prev = node1;
 
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+node1->next = node2->next;
+node2->prev = node1->prev;
+node1->prev = node2;
+node2->next = node1;
 }
